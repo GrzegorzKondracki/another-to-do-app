386b88190f95be1412311940552fdd81465239ec
GrzegorzKondracki <g.kondracki@outlook.com>
2020-09-14 22:50:19 +0200

    css for input date

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,21 +3,39 @@ import '../css/Task.scss';
 
 const Task = (props) => {
 
-    const style = {
+    const importantStyle = {
         color: 'rgb(206 4 4)',
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+    }
+    const doneStyle = {
+        textDecoration: 'line-through'
     }
 
-    const { id, text, deadline, important } = props.task;
 
-    return (
-        <div className="task">
-            <p style={important ? style : null}>{text}</p>
-            <p>deadline {deadline}</p>
-            <button onClick={() => props.done(id)} >done</button>
-            <button onClick={() => props.delete(id)} >delete</button>
-        </div>
-    );
+
+    const { id, text, deadline, important, active } = props.task;
+
+    if (active) {
+        return (
+            <div className="task">
+                <p style={important ? importantStyle : null}>{text}</p>
+                <p style={active ? null : doneStyle}>deadline {deadline}</p>
+                <button onClick={() => props.done(id)} >done</button>
+                <button onClick={() => props.delete(id)} >delete</button>
+            </div>
+        );
+    }
+
+    else {
+        return (
+            <div className="task">
+                <p style={active ? null : doneStyle}>{text}</p>
+                <p style={active ? null : doneStyle}>deadline {deadline}</p>
+                <button onClick={() => props.delete(id)} >delete</button>
+            </div>
+        );
+    }
+
 }
 
 export default Task;
