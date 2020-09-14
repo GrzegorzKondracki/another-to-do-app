90e10f1d4a2074d2e3a6a0a8b022cb41f8a89999
GrzegorzKondracki <g.kondracki@outlook.com>
2020-09-14 23:06:49 +0200

    Updates

---
+++ precache-manifest.5c122e82ba0769c6d66a9635ac7822c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b576e00e1747dbe84fb36870ac990d",
+    "url": "/another-to-do-app/index.html"
+  },
+  {
+    "revision": "da22130b2a4447cd6c66",
+    "url": "/another-to-do-app/static/css/main.06a6072e.chunk.css"
+  },
+  {
+    "revision": "8e48beef742ce950b219",
+    "url": "/another-to-do-app/static/js/2.b5f485d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/another-to-do-app/static/js/2.b5f485d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da22130b2a4447cd6c66",
+    "url": "/another-to-do-app/static/js/main.8074cf4e.chunk.js"
+  },
+  {
+    "revision": "bd9b502525e64af85d49",
+    "url": "/another-to-do-app/static/js/runtime-main.97fda424.js"
+  }
+]);
